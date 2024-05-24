c7e7fbf223a8c08e379f914dd50238514c3b7edd
gabrielioanpavel <pavelgabrielioan@gmail.com>
2024-05-24 22:03:46 +0300

    Am adaugat functiile pentru a calcula procentajele cerute si am scris codul pentru a afisa seturile de rezultate.

--- examine.py
+++ examine.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def listData(df: pd.DataFrame) -> list:
     dataList = []
@@ -11,5 +13,23 @@ def listData(df: pd.DataFrame) -> list:
         if i == True:
             dup = False
             break
-    dataList.append(dup)                            # Numar de duplicate
+    dataList.append(dup)                            # Daca exista duplicate
     return dataList
+
+def prcS(df: pd.DataFrame):
+    y = df['Survived'].sum()
+    l = len(df['Survived'])
+    return round(y/l*100, 2), round((l-y)/l*100, 2)
+
+def prcPclass(df: pd.DataFrame):
+    c1 = sum(df['Pclass'] == 1)
+    c2 = sum(df['Pclass'] == 2)
+    c3 = sum(df['Pclass'] == 3)
+    l = len(df['Pclass'])
+    return round(c1/l*100, 2), round(c2/l*100, 2), round(c3/l*100, 2)
+
+def prcSex(df: pd.DataFrame):
+    m = sum(df['Sex'] == 'male')
+    f = sum(df['Sex'] == 'female')
+    l = len(df['Sex'])
+    return round(m/l*100, 2), round(f/l*100, 2)
